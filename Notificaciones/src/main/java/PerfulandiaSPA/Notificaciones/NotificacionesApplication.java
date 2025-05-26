82a51bd9c49a83d97bc11880a632f638bf1474c4
vichomnz <vt.munoz@duocuc.cl>
2025-05-26 14:09:43 -0400

    Initlzr Noti

---
+++ Notificaciones/src/main/java/PerfulandiaSPA/Notificaciones/NotificacionesApplication.java
@@ -0,0 +1,13 @@
+package PerfulandiaSPA.Notificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionesApplication.class, args);
+	}
+
+}
---
+++ Notificaciones/src/test/java/PerfulandiaSPA/Notificaciones/NotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package PerfulandiaSPA.Notificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
